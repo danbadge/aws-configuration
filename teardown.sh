68b563e571d2086a43306214220391787dc55eac
Daniel Watts <dwatts@eudwatts.local>
2014-12-16 17:39:05 +0000

    delete everything from s3 before tearing down

--- teardown.sh
+++ teardown.sh
@@ -1,2 +1,5 @@
 aws cloudformation delete-stack --stack-name network-stack
+
+S3_BUCKET=`aws s3api list-buckets | grep -Eo 'deployment.*[^"]'`
+aws s3 rb s3://${S3_BUCKET} --force
 aws cloudformation delete-stack --stack-name deployment-setup 
